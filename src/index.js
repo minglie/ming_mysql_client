5ef3ccb6afde528a67abf2e15bbc754b8cb6ced8
minglie <934031452@qq.com>
2018-11-06 20:36:58 +0800

    V0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
-var M=require("ming_node");M.log_file_enable=false; //²»ÓÃ´òÓ¡ÈÕÖ¾µ½ÎÄ¼şvar path=require('path');var Db=require("./modules/ming_mysql");var app=M.server();app.listener(8889);app.set("views",path.join(__dirname, "./static"));var database=Db.myDbconfig.database;var tableName=0;app.get(`/`,async function (req,res) {    app.redirect("/index.html",req,res);})app.get(`/t/:tableName`,async function (req,res) {    tableName=req.param.tableName;    app.redirect("/index.html",req,res);})app.get(`/doSql`,async function (req,res) {    console.log(req.param.sql);    var tableNameList= await Db.doSql(req.param.sql);    res.send(M.result(tableNameList));})app.get(`listAlltableName`,async function (req,res) {    var tableNameList= await Db.doSql("show tables");    tableNameList=tableNameList.map(u=>Object.values(u)[0])    res.send(M.result(tableNameList))})app.get(`/getTableInfo`,async function (req,res) {    if(tableName==0){        res.send(M.result("no table"))    }else{        let r=await Db.doSql(`SELECT column_name,column_type,column_comment FROM information_schema.COLUMNS WHERE TABLE_NAME='${tableName}' and TABLE_SCHEMA='${database}'`)        res.send(M.result(r.map(u=>{            result={};        result.column_name=u.column_name;        result.column_comment=u.column_comment;        return result;    })))    }})//¸ù¾İidÉ¾³ıapp.get(`/delete`,async function (req,res) {    console.log(req.param["ids[]"])    var r;    if(Array.isArray(req.param["ids[]"])){        r=await Db.doSql(`delete from ${tableName} where id in (${req.param["ids[]"].join(",")})`)    }else {        r=await Db.doSql(`delete from ${tableName} where id in (${req.param["ids[]"]})`)    }    if(r.affectedRows>0){        res.send(M.result("É¾³ı³É¹¦"));    }else{        res.send(M.result("É¾³ıÊ§°Ü"));    }})//·ÖÒ³²éÑ¯app.post(`/listByPage`,async function (req,res) {    var limit=req.param.rows;    var start=(req.param.page - 1) * limit;    var rows=await Db.doSql(`    select * from ${tableName} t1  limit  ${start},${limit}`);    var total=await Db.doSql(`    select count(1) ct from ${tableName} t1 `);    total=total[0].ct;    res.send(JSON.stringify({rows,total}));})
+var M=require("ming_node");
+M.log_file_enable=false; //ä¸ç”¨æ‰“å°æ—¥å¿—åˆ°æ–‡ä»¶
+var path=require('path');
+var Db=require("./modules/ming_mysql");
+var app=M.server();
+app.listener(8889);
+app.set("views",path.join(__dirname, "./static"));
+
+
+var database=Db.myDbconfig.database;
+var tableName=0;
+
+
+app.get(`/`,async function (req,res) {
+    app.redirect("/index.html",req,res);
+})
+app.get(`/t/:tableName`,async function (req,res) {
+    tableName=req.param.tableName;
+    app.redirect("/index.html",req,res);
+})
+app.get(`/doSql`,async function (req,res) {
+    console.log(req.param.sql);
+    var tableNameList= await Db.doSql(req.param.sql);
+    res.send(M.result(tableNameList));
+})
+app.get(`listAlltableName`,async function (req,res) {
+    var tableNameList= await Db.doSql("show tables");
+    tableNameList=tableNameList.map(u=>Object.values(u)[0])
+    res.send(M.result(tableNameList))
+})
+app.get(`/getTableInfo`,async function (req,res) {
+
+    if(tableName==0){
+        res.send(M.result("no table"))
+    }else{
+        let r=await Db.doSql(`SELECT column_name,column_type,column_comment FROM information_schema.COLUMNS WHERE TABLE_NAME='${tableName}' and TABLE_SCHEMA='${database}'`)
+        res.send(M.result(r.map(u=>{
+            result={};
+        result.column_name=u.column_name;
+        result.column_comment=u.column_comment;
+        return result;
+    })))
+    }
+})
+//æ ¹æ®idåˆ é™¤
+app.get(`/delete`,async function (req,res) {
+    console.log(req.param["ids[]"])
+    var r;
+    if(Array.isArray(req.param["ids[]"])){
+        r=await Db.doSql(`delete from ${tableName} where id in (${req.param["ids[]"].join(",")})`)
+    }else {
+        r=await Db.doSql(`delete from ${tableName} where id in (${req.param["ids[]"]})`)
+    }
+    if(r.affectedRows>0){
+        res.send(M.result("åˆ é™¤æˆåŠŸ"));
+    }else{
+        res.send(M.result("åˆ é™¤å¤±è´¥"));
+    }
+})
+//åˆ†é¡µæŸ¥è¯¢
+app.post(`/listByPage`,async function (req,res) {
+    var limit=req.param.rows;
+    var start=(req.param.page - 1) * limit;
+    var rows=await Db.doSql(`
+    select * from ${tableName} t1  limit  ${start},${limit}`);
+    var total=await Db.doSql(`
+    select count(1) ct from ${tableName} t1 `);
+    total=total[0].ct;
+    res.send(JSON.stringify({rows,total}));
+})
--- src/modules/ming_mysql.js
+++ src/modules/ming_mysql.js
@@ -1,1 +1,156 @@
-var mysql  = require('mysql');var path=require('path');var M=require("ming_node")var applicationConfig;try{    applicationConfig=M.getObjByFile(path.join(__dirname, "../../applicationConfig.json"));}catch (e){    applicationConfig={        "myDbconfig":{            "host"     : "127.0.0.1",            "user"     : "root",            "password" : "123456",            "port"     : "3306",            "database" : "aos_agri_decision"        }    }}var myDbconfig=applicationConfig.myDbconfig;var Db = mysql.createConnection(myDbconfig);Db.myDbconfig=myDbconfig;Db.display_sql_enable=true;Db.do_sql_enable=true;Db.connect();Db.getAddObjectSql=function(tableName,obj){    var fields="(";    var values="(";    for(let field in obj){        fields+=field+",";        values+=`'${obj[field]}'`+",";    }    fields=fields.substr(0,fields.lastIndexOf(","));    values=values.substr(0,values.lastIndexOf(","));    fields+=")";    values+=")";    let sql = "insert into "+tableName+fields+" values "+values;    return sql;}/** *Éú³ÉsqlÊ±¶Ô²»ºÏ·¨×Ö¶Î¹ýÂË */Db.getAddObjectSql1=function(tableName,obj){    var fields="(";    var values="(";    for(let field in obj){        fields+=field+",";        values+=`'${obj[field].replace(/'|"/g,"")}'`+",";    }    fields=fields.substr(0,fields.lastIndexOf(","));    values=values.substr(0,values.lastIndexOf(","));    fields+=")";    values+=")";    let sql = "insert into "+tableName+fields+" values "+values;    return sql;}Db.getDeleteObjectSql=function(tableName,obj){    var fields=[];    for(let field in obj){        fields.push(field);    }    let sql=`delete from ${tableName} where ${fields.map(u=> u+"='"+obj[u]+"'")}`;    sql=sql.replace(/,/g," and ")    return sql;}Db.getUpdateObjectSql=function(tableName,obj,caseObj){    var fields=[];    for(let field in obj){        if(field !="id")            fields.push(field);    }    let sql="";    if(!caseObj){        sql=`update ${tableName} set ${fields.map(u =>u + "='" + obj[u]+ "'")} where id=${obj.id}`;    }else{        var caseObjfields=[];        for(let caseObjfield in caseObj){            caseObjfields.push(caseObjfield)        }        sql=`update ${tableName} set ${fields.map(u =>u + "='" + obj[u]+ "'")} where ${caseObjfields.map(u=> u+"='"+caseObj[u]+"'").join(" and ")}`;    }    return sql;}Db.getQueryObjectSql=function(tableName,obj){    var fields=[];    for(let field in obj){        fields.push(field);    }    let sql = `select * from ${tableName} where ${fields.map(u=> u+"='"+obj[u]+"'")}`;    sql=sql.replace(/,/g," and ")    return sql;}function doSql(sql){    var promise = new Promise(function(reslove,reject){        if(Db.display_sql_enable) M.log(sql+";")        if(Db.do_sql_enable){            Db.query(sql,                function (err, result) {                    if(err)console.error(err);                    reslove(result);                });        }    })    return promise;}Db.doSql=doSql;function addObj(tableName,obj){    var sql=Db.getAddObjectSql(tableName,obj);    return  Db.doSql(sql);}function deleteObj(tableName,obj){    var sql=Db.getDeleteObjectSql(tableName,obj);    return  Db.doSql(sql);}function updateObj(tableName,obj,caseObj){    var sql=Db.getUpdateObjectSql(tableName,obj,caseObj);    return  Db.doSql(sql);}function getByObj(tableName,obj){    var sql=Db.getQueryObjectSql(tableName,obj);    return  Db.doSql(sql);}Db.addObj=addObj;Db.deleteObj=deleteObj;Db.updateObj=updateObj;Db.getByObj=getByObj;module.exports=Db;if(0)    +async function(){    k= await Db.getUpdateObjectSql("aos_sys_user",{id:7,user_name:'zs',user_birth:'2015-11-02',user_salary:21},{name:"zs",age:8,kk:"dfsasf"})    console.log(JSON.stringify(k))}();
+var mysql  = require('mysql');
+var path=require('path');
+var M=require("ming_node")
+
+var applicationConfig;
+
+
+try{
+    applicationConfig=M.getObjByFile(path.join(__dirname, "../../applicationConfig.json"));
+}catch (e){
+    applicationConfig={
+        "myDbconfig":{
+            "host"     : "127.0.0.1",
+            "user"     : "root",
+            "password" : "123456",
+            "port"     : "3306",
+            "database" : "aos_agri_decision"
+        }
+    }
+}
+
+
+var myDbconfig=applicationConfig.myDbconfig;
+
+
+var Db = mysql.createConnection(myDbconfig);
+Db.myDbconfig=myDbconfig;
+Db.display_sql_enable=true;
+Db.do_sql_enable=true;
+Db.connect();
+
+Db.getAddObjectSql=function(tableName,obj){
+    var fields="(";
+    var values="(";
+    for(let field in obj){
+        fields+=field+",";
+        values+=`'${obj[field]}'`+",";
+    }
+    fields=fields.substr(0,fields.lastIndexOf(","));
+    values=values.substr(0,values.lastIndexOf(","));
+    fields+=")";
+    values+=")";
+    let sql = "insert into "+tableName+fields+" values "+values;
+    return sql;
+}
+
+
+
+/**
+ *ç”Ÿæˆsqlæ—¶å¯¹ä¸åˆæ³•å­—æ®µè¿‡æ»¤
+ */
+Db.getAddObjectSql1=function(tableName,obj){
+    var fields="(";
+    var values="(";
+    for(let field in obj){
+        fields+=field+",";
+        values+=`'${obj[field].replace(/'|"/g,"")}'`+",";
+    }
+    fields=fields.substr(0,fields.lastIndexOf(","));
+    values=values.substr(0,values.lastIndexOf(","));
+    fields+=")";
+    values+=")";
+    let sql = "insert into "+tableName+fields+" values "+values;
+    return sql;
+}
+
+
+Db.getDeleteObjectSql=function(tableName,obj){
+    var fields=[];
+    for(let field in obj){
+        fields.push(field);
+    }
+    let sql=`delete from ${tableName} where ${fields.map(u=> u+"='"+obj[u]+"'")}`;
+    sql=sql.replace(/,/g," and ")
+    return sql;
+}
+
+Db.getUpdateObjectSql=function(tableName,obj,caseObj){
+    var fields=[];
+    for(let field in obj){
+        if(field !="id")
+            fields.push(field);
+    }
+    let sql="";
+    if(!caseObj){
+        sql=`update ${tableName} set ${fields.map(u =>u + "='" + obj[u]+ "'")} where id=${obj.id}`;
+    }else{
+        var caseObjfields=[];
+        for(let caseObjfield in caseObj){
+            caseObjfields.push(caseObjfield)
+        }
+        sql=`update ${tableName} set ${fields.map(u =>u + "='" + obj[u]+ "'")} where ${caseObjfields.map(u=> u+"='"+caseObj[u]+"'").join(" and ")}`;
+    }
+
+    return sql;
+}
+
+
+Db.getQueryObjectSql=function(tableName,obj){
+    var fields=[];
+    for(let field in obj){
+        fields.push(field);
+    }
+    let sql = `select * from ${tableName} where ${fields.map(u=> u+"='"+obj[u]+"'")}`;
+    sql=sql.replace(/,/g," and ")
+    return sql;
+}
+
+
+
+function doSql(sql){
+    var promise = new Promise(function(reslove,reject){
+        if(Db.display_sql_enable) M.log(sql+";")
+        if(Db.do_sql_enable){
+            Db.query(sql,
+                function (err, result) {
+                    if(err)console.error(err);
+                    reslove(result);
+                });
+        }
+    })
+    return promise;
+}
+
+Db.doSql=doSql;
+
+function addObj(tableName,obj){
+    var sql=Db.getAddObjectSql(tableName,obj);
+    return  Db.doSql(sql);
+}
+function deleteObj(tableName,obj){
+    var sql=Db.getDeleteObjectSql(tableName,obj);
+    return  Db.doSql(sql);
+}
+function updateObj(tableName,obj,caseObj){
+    var sql=Db.getUpdateObjectSql(tableName,obj,caseObj);
+    return  Db.doSql(sql);
+}
+function getByObj(tableName,obj){
+    var sql=Db.getQueryObjectSql(tableName,obj);
+    return  Db.doSql(sql);
+}
+
+Db.addObj=addObj;
+Db.deleteObj=deleteObj;
+Db.updateObj=updateObj;
+Db.getByObj=getByObj;
+module.exports=Db;
+
+if(0)
+    +async function(){
+    k= await Db.getUpdateObjectSql("aos_sys_user",{id:7,user_name:'zs',user_birth:'2015-11-02',user_salary:21},{name:"zs",age:8,kk:"dfsasf"})
+    console.log(JSON.stringify(k))
+}();
+
+
